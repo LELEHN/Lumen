d764958861299c622d746db337807264c2acd752
Lethicia Nobre <lethicianobre2@gmail.com>
2026-05-21 14:56:22 -0300

    ip

--- FrontEnd/src/services/api.js
+++ FrontEnd/src/services/api.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-const API_URL = "http://192.168.15.5:5010"; // seu IP
-=======
-const API_URL = "http://192.168.0.183:5010"; // seu IP
->>>>>>> 14ec8342166a1988034d35168ca1478847275e77
+const API_URL = "http://192.168.15.7:5010"; // seu IP
 
 export async function apiFetch(endpoint, options) {
-    
-  const resp = await fetch(`${API_URL}${endpoint}`, options);
+const resp = await fetch(`${API_URL}${endpoint}`, options);
 
   const data = await resp.json();
 
